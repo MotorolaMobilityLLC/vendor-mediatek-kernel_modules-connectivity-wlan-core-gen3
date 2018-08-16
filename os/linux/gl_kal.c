f234b8822fbec6f245bd9db37032843065e509eb
Qiang guo <qiang.guo@mediatek.com>
2018-08-16 12:25:00 +0800

    [ALPS04065415] wlan: drop error tail packet
    [Detail]
    KE due to error packet with too big tail.
    
    [Solution]
    1. Drop packet if the tail is too big than end.
    2. Remove assert.
    
    MTK-Commit-Id: df8f779acc21c9135df883375ea91220f850074f
    
    Change-Id: I39081a2bec1ab6d0c24e2948d654793c3d531d87
    Signed-off-by: Qiang guo <qiang.guo@mediatek.com>
    CR-Id: ALPS04065415
    Feature: [Module]Wi-Fi Driver
    (cherry picked from commit 51ccf0a55aae517dc007671bf40642e806aa2108)
    (cherry picked from commit 08d3baa905c19bc73c26d501ee6359498db4f2d3)

--- os/linux/gl_kal.c
+++ os/linux/gl_kal.c
@@ -800,6 +800,14 @@ kalProcessRxPacket(IN P_GLUE_INFO_T prGlueInfo, IN PVOID pvPacket, IN PUINT_8 pu
 	skb_reset_tail_pointer(skb);
 	skb_trim(skb, 0);
 
+	if (skb->tail > skb->end) {
+		DBGLOG(RX, ERROR,
+		       "[skb:0x%p][skb->len:%d][skb->protocol:0x%02X] tail:%p, end:%p, data:%p\n",
+			(PUINT_8) skb, skb->len, skb->protocol, skb->tail, skb->end, skb->data);
+		DBGLOG_MEM32(RX, ERROR, (PUINT_32) skb->data, skb->len);
+		return WLAN_STATUS_FAILURE;
+	}
+
 	/* Put data */
 	skb_put(skb, u4PacketLen);
 
