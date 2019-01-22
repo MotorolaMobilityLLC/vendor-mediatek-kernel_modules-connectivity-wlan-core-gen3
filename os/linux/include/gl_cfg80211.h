4fe9d80bd4d4f0f23f15281d12ffe84ef82a2a54
Xiang Wu <xiang.wu@mediatek.com>
2019-01-22 19:26:07 +0800

    [ALPS04077045] wlan: Porting change_virtual_intf
    In kernel4.12 or above：
    1. change_virtual_intf has no param "flags"
    2. WLAN_AKM_SUITE_FT_8021X redefined.
    2. WLAN_AKM_SUITE_FT_PSK redefined.
    
    MTK-Commit-Id: 47c13253bef95ba46f8f02eb1d272c61ce631c02
    
    Change-Id: I5e763b33f86e1c8d41df2e7bc74c5866a555c1d0
    Signed-off-by: Xiang Wu <xiang.wu@mediatek.com>
    CR-Id: ALPS04077045
    Feature: [Module]Wi-Fi Driver

--- os/linux/include/gl_cfg80211.h
+++ os/linux/include/gl_cfg80211.h
@@ -180,9 +180,15 @@ typedef enum _ENUM_TESTMODE_STA_STATISTICS_ATTR {
 ********************************************************************************
 */
 /* cfg80211 hooks */
+#if (KERNEL_VERSION(4, 12, 0) <= LINUX_VERSION_CODE)
+int
+mtk_cfg80211_change_iface(struct wiphy *wiphy,
+			  struct net_device *ndev, enum nl80211_iftype type, struct vif_params *params);
+#else
 int
 mtk_cfg80211_change_iface(struct wiphy *wiphy,
 			  struct net_device *ndev, enum nl80211_iftype type, u32 *flags, struct vif_params *params);
+#endif
 
 int
 mtk_cfg80211_add_key(struct wiphy *wiphy,
